694dea30d159a561c72b472a7bf910afbfaa7600
periwal45 <vin.peri04@gmail.com>
2024-09-14 17:20:46 +0100

    Refresh_changes
    Commit

--- primary_screen.R
+++ primary_screen.R
@@ -263,7 +263,6 @@ nrow(plate_layout) #14,976
 # write.table(fitted_values, file = "GC_fitted_model_values", sep = "\t", quote = FALSE, row.names = FALSE)
 # write.table(gc_fit_params, file = "GC_fit_params", sep = "\t", quote = FALSE, row.names = FALSE)
 
-
 ############################### Read fitted values & plot
 #' #read fitted values of models
 #' fits<-data.frame(read.table("GC_fitted_model_values", header = TRUE, sep = '\t', stringsAsFactors = FALSE))
@@ -317,6 +316,241 @@ nrow(plate_layout) #14,976
 #' CairoSVG(file=paste("../Figures/NT5007_noisy.svg"), width = 4, height = 3, bg = "white")
 #' merge_plate_layout_fits %>% dplyr::group_by(Bug_ID,Plate_no,compound,Replicate_no) %>% 
 #'   filter(Bug_ID == 'NT24007' & compound %in% c('DMSO') & Plate_no %in% c('plate1')) %>%
+#'   ggplot(aes(x=time, y=fitted, group=color)) + geom_line(aes(color=Replicate_no), size=0.4) + scale_color_uchicago() +
+#'   geom_point(aes(y=OD, color=Replicate_no), size=0.5) + facet_wrap("compound", scales = "free") +  
+#'   scale_x_continuous(name = "Time (Hours)", limits = c(0,25)) + scale_y_continuous(name="bg corrected OD") + 
+#'   th + theme_minimal()
+#' dev.off()
+#' #'H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'
+
+# #merge all replicates of a bug
+# # Merge files across all replicates of a strain (.annot files)
+# dir.names<-list.dirs(path = '.', recursive = FALSE)
+# dir.names
+# 
+# for(d in 1:length(dir.names)){
+#   
+#   if(str_detect(dir.names[d], "./NT")){
+#     
+#     all_rep<-do.call(rbind, lapply(a<-list.files(path=dir.names[d], pattern="\\.annot$", full.names = TRUE), function(i){
+#       read.table(i,header=TRUE, sep="\t", stringsAsFactors = FALSE)}))
+#     write.table(all_rep, file = paste0(dir.names[d],"_all_replicates.merged"), sep = "\t", quote = FALSE, row.names = FALSE)
+#     
+#   }
+#   
+# }
+# 
+# #read all files with .merged extension
+# file.merged<-list.files(path = '.', pattern = ".merged$") #recursive reads through all subfolders and files
+# file.merged
+# 
+# #create an empty data frame to store all raw points and fitted points
+# fitted_values<-data.frame(stringsAsFactors = FALSE)
+# 
+# #create an empty data frame to store all computed model parameters
+# gc_fit_params<-data.frame(stringsAsFactors = FALSE)
+# 
+# bug_min_reads<-data.frame(Bug_ID = character(), Replicate_no = character(), count_tp = double(), stringsAsFactors = FALSE)
+# 
+# #loop for each file, fits logistic curve, annotates
+# for(j in 1:length(file.merged)){
+#   
+#   #read file/table
+#   in_file<-tools::file_path_sans_ext(file.merged[j]) #read filename w/o extension
+#   in_file
+#   
+#   Y<-data.frame(read.table(file.merged[j], header = TRUE, sep = '\t', stringsAsFactors = FALSE))
+#   head(Y)
+#   lapply(Y, class)
+#   
+#   Y[, 4:ncol(Y)] <- lapply(4:ncol(Y), function(x) as.numeric(Y[[x]]))
+#   
+#   #count time points in all replicates, all should have same time point reading else trim if needed
+#   
+#   time_points<-Y %>% dplyr::group_by(Bug_ID,Replicate_no) %>%
+#     summarise(count_tp = max(time))
+#   head(time_points)
+#   time_points<-time_points %>% mutate(min_tp = min(count_tp))
+#   min_tp<-min(time_points$count_tp)
+#   min_tp
+#   
+#   bug_min_reads<-rbind(bug_min_reads, data.frame(time_points))
+#   bug_min_reads
+#   
+#   Z<-Y %>% dplyr::group_by(Bug_ID,Replicate_no,Plate_no) %>%
+#     filter(time <= min_tp)
+#   
+#   head(Z)
+#   nrow(Z)
+#   
+#   write.table(Z, file = paste0("trim_points/",in_file,".trim"), sep = "\t", quote = FALSE, row.names = FALSE)
+#   
+#   unique_rep<-unique(Z$Replicate_no)
+#   unique_rep
+#   
+#   unique_plate<-unique(Z$Plate_no)
+#   unique_plate
+#   
+#   #loop plate wise and then well wise (col_name) to fit growth curve on each well, later merge all data in a data frame
+#   for(j in 1:length(unique_plate)){
+#     
+#     dat<-Z %>% filter(Plate_no == unique_plate[j])
+#     dat
+#     
+#     if (dim(dat)[1] != 0){ #if missing plate
+#       
+#       for(k in 1:length(unique_rep)){
+#         
+#         plate_dat<-dat %>% filter(Replicate_no == unique_rep[k])
+#         plate_dat
+#         
+#         if (dim(plate_dat)[1] != 0){ #if missing replicate eg NT5022, plate16 has no rep1
+#           
+#           bug<-plate_dat[1,1]
+#           bug
+#           rep<-plate_dat[1,2]
+#           rep
+#           plate<-plate_dat[1,3]
+#           plate
+# 
+#           data<-plate_dat[,-(1:3)]
+#           data
+# 
+#           #fit model to each well of a plate and save the fitted model
+#           for(col_name in names(data)){
+#             
+#             col_name
+#             
+#             if(col_name != "time"){
+#               
+#               current_well<-data[, c("time",col_name)]
+#               current_well
+#               min_OD<-min((current_well[, col_name])[1,1])
+#               min_OD
+#               max_OD<-max(current_well[, col_name])
+#               max_OD
+#               
+#               #do background correction using min OD of each well
+#               current_well[,col_name]<-current_well[,col_name] - min_OD
+#               current_well[,col_name]
+#               current_well
+#               
+#                #each time create a new variable for each well
+#               gc_fit<-SummarizeGrowth(data_t = current_well[,"time"], data_n = current_well[,col_name])
+#               #saveRDS(gc_fit, file = paste0("/Users/vinitaperiwal/GrowthCurver/models/",ID,"_",rep,"_",plate,"_",drug,"_",col_name,".rds"))
+#               
+#               gc_fit
+#               
+#               #create a data frame of raw values and fitted values
+#               mod_t<-data.frame(matrix(unlist(gc_fit$data$t)))
+#               mod_t
+#               mod_N<-data.frame(matrix(unlist(gc_fit$data$N)))
+#               mod_N
+#               
+#               if(gc_fit$vals$k != 0 & gc_fit$vals$n0 != 0 & gc_fit$vals$r != 0){
+#                 
+#                 mod<-cbind(mod_t, mod_N, gc_fit$model$m$fitted()) #m is the model object with all fitted and residual values
+#                 
+#               }else{
+#                 
+#                 mod<-cbind(mod_t, mod_N, gc_fit$vals$r)
+#                 
+#               }
+#               
+#               colnames(mod)<-c("time","OD","fitted")
+#               mod
+#               
+#               #add annotation to each row
+#               annot_mod<-cbind(bug,rep,plate,col_name,mod)
+#               annot_mod
+#               
+#               fitted_values<-rbind(fitted_values, annot_mod)
+#               fitted_values
+#               
+#               fitted_param<-data.frame(cbind(bug,rep,plate,col_name,gc_fit$vals$k,gc_fit$vals$k_se,gc_fit$vals$k_p,gc_fit$vals$n0,gc_fit$vals$n0_se,gc_fit$vals$n0_p,gc_fit$vals$r,gc_fit$vals$r_se,gc_fit$vals$r_p,gc_fit$vals$sigma,gc_fit$vals$df,gc_fit$vals$t_mid,gc_fit$vals$t_gen,gc_fit$vals$auc_l,gc_fit$vals$auc_e,gc_fit$vals$note,max_OD,min_OD))
+#               colnames(fitted_param)<-c("Bug_ID","Replicate_no","Plate_no","well","k","k_se","k_p","n0","n0_se","n0_p","r","r_se","r_p","sigma","df","t_mid","t_gen","auc_l","auc_e","note","max_OD","min_OD")
+#               fitted_param
+#               
+#               gc_fit_params<-rbind(gc_fit_params, fitted_param)
+#               tail(gc_fit_params)
+#                 
+#             }
+#           }
+#           
+#         }
+#         
+#       }
+#     }
+#     
+#   }
+#   
+# }
+# 
+# 
+# head(fitted_values)
+# nrow(fitted_values) #depends on time points
+# 
+# head(gc_fit_params)
+# nrow(gc_fit_params) #should be equal to number of wells: 96 for 1 plate
+# 
+# 
+# write.table(fitted_values, file = "GC_fitted_model_values", sep = "\t", quote = FALSE, row.names = FALSE)
+# write.table(gc_fit_params, file = "GC_fit_params", sep = "\t", quote = FALSE, row.names = FALSE)
+
+
+############################### Read fitted values
+#read fitted values of models
+fits<-data.frame(read.table("GC_fitted_model_values", header = TRUE, sep = '\t', stringsAsFactors = FALSE))
+head(fits)
+nrow(fits) #1,017,024
+
+merge_plate_layout_fits<-merge(plate_layout, fits, by=c("Bug_ID","Plate_no","col_name"))
+nrow(merge_plate_layout_fits) #1,017,024
+
+#View(merge_plate_layout_fits)
+
+merge_plate_layout_fits<-merge(merge_plate_layout_fits,bug_desc, by="Bug_ID")
+write.table(merge_plate_layout_fits, file = "../Figures/fitted_values", sep = "\t", quote = FALSE, row.names = FALSE)
+
+############### plot raw and fitted ODs
+#' merge_plate_layout_fits<-read.table(file = "../Figures/fitted_values", sep = "\t", header = TRUE)
+#' merge_plate_layout_fits$color<-paste0(merge_plate_layout_fits$Replicate_no,"_",merge_plate_layout_fits$tech)
+#' head(merge_plate_layout_fits)
+#' 
+#' ### plot plate wise replicates for each bug
+#' 
+#' bugs<-unique(merge_plate_layout_fits$Bug_ID)
+#' bugs
+#' 
+#' for(i in 1:length(bugs)){
+#'   
+#'   f<-merge_plate_layout_fits %>% filter(Bug_ID == bugs[i])
+#'   
+#'   plates<-unique(f$Plate_no)
+#'   plates
+#'   
+#'   for(j in 1:length(plates)){
+#'     
+#'     p<-f %>% filter(Plate_no == plates[j])
+#'     head(p)
+#'     
+#'     CairoSVG(file=paste("../Figures/", bugs[i], "_", plates[j], ".svg", sep = ""), width = 15, height = 8, bg = "white")
+#'     print(p %>% dplyr::group_by(Bug_ID,Plate_no,compound,Replicate_no) %>%
+#'             ggplot(aes(x=time, y=fitted,group=color)) + geom_line(aes(color=Replicate_no), size=0.2) + scale_color_d3(palette = "category20") +
+#'             geom_point(aes(y=OD,color=tech), size=0.000001) + facet_wrap("compound",scales = "free",ncol = 8, nrow = 6) +  
+#'             ggtitle(paste(bugs[i],p$Sp_short,plates[j],sep=":")) + scale_x_continuous(name = "Time (Hours)", limits = c(0,25)) + scale_y_continuous(name="bg corrected OD") + 
+#'             th + theme_minimal())
+#'     dev.off()
+#'     
+#'   }
+#' }
+#' 
+#' head(merge_plate_layout_fits)
+#' 
+#' # example single well plot
+#' CairoSVG(file=paste("../Figures/NT5007_noisy.svg"), width = 4, height = 3, bg = "white")
+#' merge_plate_layout_fits %>% dplyr::group_by(Bug_ID,Plate_no,compound,Replicate_no) %>% 
+#'   filter(Bug_ID == 'NT5037' & compound %in% c('DMSO') & Plate_no %in% c('plate1')) %>%
 #'   ggplot(aes(x=time, y=fitted, group=color)) + geom_line(aes(color=Replicate_no), size=0.4) + scale_color_uchicago() +
 #'   geom_point(aes(y=OD, color=Replicate_no), size=0.5) + facet_wrap("compound", scales = "free") +  
 #'   scale_x_continuous(name = "Time (Hours)", limits = c(0,25)) + scale_y_continuous(name="bg corrected OD") + 
@@ -398,7 +632,11 @@ head(params)
 ##### plate-based normalization (median of controls)
 PC<-params %>% filter(compound == 'DMSO') %>% dplyr::group_by(Bug_ID,Plate_no,Replicate_no,Phyla,Sp_short) %>%
   mutate(ctrl_med = round(median(auc_l),digits = 2))
+
 #View(PC)
+
+View(PC)
+
 dim(PC)
 
 normAUCl_PC<-merge(params,PC)
